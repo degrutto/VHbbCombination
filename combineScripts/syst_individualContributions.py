7447cfe60618ef2da63869e06b77ed884ab22425
michele de gruttola <michele.de.gruttola@cern.ch>
2018-01-30 21:59:50 +0100

    Update syst_individualContributions.py

--- combineScripts/syst_individualContributions.py
+++ combineScripts/syst_individualContributions.py
@@ -18,7 +18,7 @@ for g in groups:
     if str('SFsdata') in g :
         print 'for rateParams'
         print g
-        os.system("combineTool.py -M ProfileLikelihood -m 125 --signif --pvalue -t -1 --expectSignal=1 vhbb_2016_13TeV_norateParam.txt -n prefit%s   --job-mode lxbatch --task-name lxbatch_mdg_prefit%s  --sub-opts='-q 1nd -G CMS_CERN01_YODA'" % (str(g), str(g)) ) 
+        os.system("combineTool.py -M ProfileLikelihood -m 125 --signif --pvalue -t -1 --expectSignal=1 vhbb_2016_13TeV_onlyrateParam.txt -n prefit%s   --job-mode lxbatch --task-name lxbatch_mdg_prefit%s  --sub-opts='-q 1nd -G CMS_CERN01_YODA'" % (str(g), str(g)) ) 
 
         os.system("combineTool.py -M MaxLikelihoodFit -v 2 -m 125  -t -1 --expectSignal=1 --robustFit=1  --stepSize=0.01 --rMin=-5 --rMax=5 vhbb_2016_13TeV_norateParam.txt -n mlfit%s   --job-mode lxbatch --task-name lxbatch_mdg_mlfit%s    --minimizerAlgo=Minuit  --sub-opts='-q 1nd -G CMS_CERN01_YODA'" % ( str(g), str(g)) ) 
         
